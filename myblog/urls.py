29301628920f1a6ab39580f912064acc15ab0b47
Artem <artem.tokar13@gmail.com>
2021-06-07 19:22:52 +0200

    07062021

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,6 +1,18 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import MainView
+from .views import MainView, PostDetailView, SignUpView, SignInView, FeedBackView, SuccessView, SaerchResultView
+from django.conf.urls import url
+
+from django.conf import settings
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signin/', SignInView.as_view(), name='signin'),
+    path('signout/', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='signout',),
+    path('contact/', FeedBackView.as_view(), name='contact'),
+    path('contact/success/', SuccessView.as_view(), name='success'),
+    path('search/', SaerchResultView.as_view(), name='search'),
+    path('<slug>/', PostDetailView.as_view(), name='post_detail'),
+
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,7 +1,11 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from django.core.paginator import Paginator
 from .models import Post
+from .forms import SignUpForm, SignInForm, FeedBackForm
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponseRedirect, HttpResponse
 
 
 class MainView(View):
@@ -19,3 +23,90 @@ class MainView(View):
                 'page_obj': page_obj
             }
         )
+
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'myblog/post_detail.html', context={
+            'post': post
+        })
+
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignUpForm()
+        return render(request, 'myblog/signup.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'myblog/signup.html', context={
+            'form': form,
+        })
+
+
+class SignInView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignInForm()
+        return render(request, 'myblog/signin.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'myblog/signin.html', context={
+            'form': form,
+        })
+
+
+class FeedBackView(View):
+    def get(self, request, *args, **kwargs):
+        form = FeedBackForm()
+        return render(request, 'myblog/contact.html', context={
+            'form': form,
+            'title': 'Write me'
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = FeedBackForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            from_email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(f'From {name} | {subject}', message, from_email, ['artem.tokar13@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header')
+            return HttpResponseRedirect('success')
+        return render(request, 'myblog/contact.html', context={
+            'form': form,
+        })
+
+
+class SuccessView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'myblog/success.html', context={
+            'title': 'Thank you!'
+        })
+
+
+class SaerchResultView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'myblog/search.html', context={
+            'title': 'Search'
+        })
